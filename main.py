c413889b8d750e10e6435909c04ad5e869eb2f2c
Aubrey <clausse.aubrey@gmail.com>
2018-02-23 14:06:03 +0100

    clean version

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ if __name__ == "__main__":
     # Run code for several velocity:
     L = 10;
     M = 1;
-    for v_init in np.arange(1, 6, 1):
+    array = [1,2,3,4,5,6];
+    for v_init in array:
 
         # Define init and final state
         x_init = np.array([0,-L, 0, v_init]);
@@ -15,4 +16,4 @@ if __name__ == "__main__":
         sol_param = find_control(x_init, x_final, M);
         
         # Draw or save the trajectories
-        generate_traj(sol_param, x_init, x_final, M, False, True, 'resuts');
+        generate_traj(sol_param, x_init, x_final, M, False, True, 'results');
